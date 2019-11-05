444e9c22653e78a9099436051b5960268b4e5af6
student <student@safebear.co.uk>
2019-11-05 15:55:58 +0000

    Chromedriver complete

--- src/test/java/co/uk/safebear/Stepdefs.java
+++ src/test/java/co/uk/safebear/Stepdefs.java
@@ -1,6 +1,7 @@
 package co.uk.safebear;
 
 import co.uk.safebear.utils.Driver;
+import cucumber.api.java.After;
 import cucumber.api.java.Before;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
@@ -11,27 +12,26 @@ import static org.junit.Assert.*;
 
 public class Stepdefs {
 
-    WebDriver driver;
+    WebDriver browser;
 
     @Before
     public void setUp(){
         // Get the driver for the browser (e.g. ChromeDriver)
-        driver = Driver.getDriver();
+        browser = Driver.getDriver();
         // Navigate to the URL of our webpage
-        driver.get(Driver.getURL());
+        browser.get(Driver.getURL());
 
     }
 
-    @When("a user searches for a job called {string}")
-    public void a_user_searches_for_a_job_called(String jobType) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new cucumber.api.PendingException();
-    }
-
-    @Then("all job results containing the word {string} are returned")
-    public void all_job_results_containing_the_word_are_returned(String jobExpectedResults) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new cucumber.api.PendingException();
+    @After
+    public void tearDown(){
+        // Pause for a couple of seconds after test has run
+        try {
+            Thread.sleep(Integer.parseInt(System.getProperty("sleep","2000")));
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        browser.quit();
     }
 
 
