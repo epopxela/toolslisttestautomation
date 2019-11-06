e9790caf3f13b1bf0f40a06f8223a3a118eda7c6
student <student@safebear.co.uk>
2019-11-06 14:40:43 +0000

    Locators

--- src/test/java/co/uk/safebear/pages/LoginPage.java
+++ src/test/java/co/uk/safebear/pages/LoginPage.java
@@ -1,4 +1,38 @@
 package co.uk.safebear.pages;
 
+import co.uk.safebear.pages.locators.LoginPageLocators;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.openqa.selenium.WebDriver;
+
+@RequiredArgsConstructor
 public class LoginPage {
+
+   //Create object
+    LoginPageLocators locators = new LoginPageLocators();
+
+    @NonNull
+    //Create a new WebDriver
+    WebDriver browser;
+
+    public String getPageTitle(){
+                return browser.getTitle();
+    }
+
+    public void enterUsername(String username){
+        browser.findElement(locators.getUsernameLocator()).sendKeys(username);
+    }
+
+    public void enterPassword(String password){
+        browser.findElement(locators.getPasswordLocator()).sendKeys(password);
+    }
+
+    public void clickLoginButton(){
+        browser.findElement(locators.getLoginbutton()).click();
+    }
+
+    public String checkForFailedLoginWarning(){
+        return browser.findElement(locators.getFailedLoginMessage()).getText();
+    }
+
 }
--- src/test/java/co/uk/safebear/pages/ToolsPage.java
+++ src/test/java/co/uk/safebear/pages/ToolsPage.java
@@ -1,8 +1,25 @@
 package co.uk.safebear.pages;
 
-import lombok.Data;
-import org.openqa.selenium.By;
+import co.uk.safebear.pages.locators.ToolsPageLocators;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.openqa.selenium.WebDriver;
 
+@RequiredArgsConstructor
 public class ToolsPage {
+
+    @NonNull
+    WebDriver browser;
+
+    //Create object
+    ToolsPageLocators locators = new ToolsPageLocators();
+
+    public String getPageTitle(){
+        return browser.getTitle();
+    }
+
+    public String checkforLoginSuccessfulMessage(){
+        return browser.findElement(locators.getSuccessfulLoginMessage()).getText();
+    }
 
 }
