68c89e35d9ee0b4bf907891e4a42c87d25ecfd63
student <student@safebear.co.uk>
2019-11-06 10:57:20 +0000

    Browser open Chrome - Running Headless

--- src/test/java/co/uk/safebear/Stepdefs.java
+++ src/test/java/co/uk/safebear/Stepdefs.java
@@ -1,29 +1,42 @@
 package co.uk.safebear;
 
+import co.uk.safebear.utils.Driver;
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import org.openqa.selenium.WebDriver;
 
 import static org.junit.Assert.*;
 
 public class Stepdefs {
 
-    @Given("I am logged out")
-    public void i_am_logged_out() {
+    WebDriver browser;
+
+    @Before
+    public void setup(){
+        browser = Driver.getDriver();
+        browser.navigate().to(Driver.getURL());
+    }
+
+    @After
+    public void teardown(){
+        browser.quit();
+
+    }
+
+    @When("a user searches for a job called {string}")
+    public void a_user_searches_for_a_job_called(String jobType) {
         // Write code here that turns the phrase above into concrete actions
         throw new cucumber.api.PendingException();
     }
 
-    @When("I enter username {string} and password {string}")
-    public void i_enter_username_and_password(String username, String password) {
+    @Then("all job results containing the word {string} are returned")
+    public void all_job_results_containing_the_word_are_returned(String jobExpectedResults) {
         // Write code here that turns the phrase above into concrete actions
         throw new cucumber.api.PendingException();
     }
 
-    @Then("the user is informed that the login is successful")
-    public void the_user_is_informed_that_the_login_is_successful() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new cucumber.api.PendingException();
-    }
 
 }
