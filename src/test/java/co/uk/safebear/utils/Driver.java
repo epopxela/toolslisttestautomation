96c8118a40cc2812f73a8c8bb1e2626fd766be3a
student <student@safebear.co.uk>
2019-11-07 14:41:50 +0000

    LoginTest via junit - no feature file

--- src/test/java/co/uk/safebear/utils/Driver.java
+++ src/test/java/co/uk/safebear/utils/Driver.java
@@ -21,6 +21,26 @@ public class Driver {
 
         switch (BROWSER.toUpperCase()) {
 
+            case ("CHROME_HEADLESS"):
+                System.out.println(" Executing on CHROME HEADLESS");
+
+                chromeoptions = new ChromeOptions();
+
+                //Set Chrome to run headlessly
+                chromeoptions.addArguments("headless");
+
+                //Make sure the window size is large and maximised
+                //So nothing disappears off screen
+                //(even in headless mode!)
+                chromeoptions.addArguments("window-size=1920,1080");
+                chromeoptions.addArguments("start-maximised");
+
+                //Set up our Chromedriver
+                WebDriverManager.chromedriver().setup();
+
+                //Return the chromedriver with the chromeOptions set
+                return new ChromeDriver(chromeoptions);
+
             case "CHROME":
 
                 //Tell the user which browser we're running our tests on
